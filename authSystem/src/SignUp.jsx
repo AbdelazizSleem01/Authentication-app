a8c49e5e3fe7aa83065c9bec7b7fea37d00d596f
AbdelazizSleem01 <134790992+AbdelazizSleem01@users.noreply.github.com>
2023-11-30 03:04:42 +0200

    demo changes

--- authSystem/src/SignUp.jsx
+++ authSystem/src/SignUp.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-
 const SignUp = () => {
 
     const [namef, setNamef] = useState()
